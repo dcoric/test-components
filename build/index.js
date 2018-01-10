d6d6c210290520f4f4ea4594433f4587a6003ef5
Denis Coric <denis85@gmail.com>
2018-01-10 14:25:02 +0100

    Fixed gitignore file

---
+++ build/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.GlButton = undefined;
+
+var _GlButton = require('./elements/GlButton');
+
+var _GlButton2 = _interopRequireDefault(_GlButton);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.GlButton = _GlButton2.default;
